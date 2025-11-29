852d097fd26c4df617e91d3d0edc56823f7fc0cd
Viih cavalcante <viih.cavalcante@alu.ufc.br>
2025-11-28 21:23:55 -0300

    estudos teorico, amanha tem a pratica pesada

--- tasks/poo/agenda/py/draft.py
+++ tasks/poo/agenda/py/draft.py
@@ -1,1 +1,10 @@
-testando commit
+class contato:
+    def _init_(self, telefone: str, favorito: bool = False):
+        self.telefone = telefone
+        self.favorito = favorito
+
+    def str(self):
+        return f"{self.nome}:{self.number}"
+    
+
+    
---
+++ tasks/poo/contato/py/draft.py
@@ -0,0 +1,7 @@
+class contato:
+    def _init_(self, nome :str, numero :int):
+        self.nome = str
+        self.numero = int
+
+    def str(self):
+        return f"{self.nome}:{self.numero}"
